97062de7b464cb858605b14cb23f5914afbdf7c7
AnnoyedD <yid061@ucsd.edu>
2016-03-02 13:03:08 -0800

    Update test.py

--- test.py
+++ test.py
@@ -10,9 +10,11 @@ log=open(logFile,'w')
 
 status,output=commands.getstatusoutput('apt-get update')
 log.write('Packages update: '+str(status)+'\n\n')
+print 'Packages updata: ',status
 
 status,output=commands.getstatusoutput('apt-cache dumpavail > '+pkgInfo)
 log.write('Dump available packakges: '+str(status)+'\n\n')
+print 'Dump available packages: ',status
 
 avail=open(pkgInfo,'r')
 
@@ -28,9 +30,11 @@ while line:
     length=len(line)
     sName=line[9:length].strip()
     log.write('Package: '+str(sName))
+    print 'Package: ',sName
 
     status,output=commands.getstatusoutput('apt-get download '+sName)
     log.write('Download package: '+str(status)+'\n')
+    print 'Download package: ',status
     
     curDir=os.getcwd()
     fileList=os.listdir(curDir)
@@ -38,7 +42,8 @@ while line:
     for f in fileList:
       if f.find('.deb')!=-1:
 	status,output=commands.getstatusoutput('dpkg-deb -x '+f+' temp')
-	log.write('Decompress package: '+str(status)+'\n')	
+	log.write('Decompress package: '+str(status)+'\n')
+	print 'Decompress package: ',status
 
 	for root,dirs,files in os.walk(curDir+'/temp'):
 	  for name in files:
@@ -55,12 +60,17 @@ while line:
 
         status,output=commands.getstatusoutput('rm -rf temp')
 	log.write('Remove temporary directory: '+str(status)+'\n')
+	print 'Remove temporary directory: ',status
         status,output=commands.getstatusoutput('rm -f '+f)
 	log.write('Remove package: '+str(status)+'\n')
+	print 'Remove package: ',status
 	#for further analysis
 	log.write('Total number of packages: '+str(totalNum)+'\n')
 	log.write('Total number of elf: '+str(totalElf)+'\n')
 	log.write('Total number of canary: '+str(canaryNum)+'\n\n')
+	print 'Total number of packages: ',totalNum
+	print 'Total numver of elf: ',totalElf
+	print 'Total number of canary: 'canaryNum
 	break
   
   line=avail.readline()
